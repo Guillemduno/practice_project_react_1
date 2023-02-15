a135406846550f61fc800f354278de952250d769
Guillem Duñó <guillemduno@gmail.com>
2023-02-15 06:00:16 +0100

    Enter a name in the form and print it in the screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/form/form";
 
 function App() {
+  const [username, setUsername] = useState("");
+
+  const saveUser = (user) => {
+    console.log(user.username);
+    console.log(user.age);
+    setUsername(user.username);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Form></Form>
+        <Form onSaveUser={saveUser}></Form>
+        <p>{username}</p>
       </header>
     </div>
   );
--- my-app/src/components/form/form.js
+++ my-app/src/components/form/form.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./form.module.css";
 import Button from "./ui/button";
-import Inform from "./ui/inForm";
 
-const Form = () => {
+const Form = (props) => {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+
+  const saveUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const saveAge = (event) => {
+    setAge(event.target.value);
+  };
+
   const saveForm = (event) => {
     event.preventDefault();
-    console.log("button clicked");
+
+    const user = {
+      username: username,
+      age: age,
+    };
+
+    props.onSaveUser(user);
+
+    setUsername("");
+    setAge("");
   };
 
   return (
     <div className={styles.form}>
       <form onSubmit={saveForm}>
-        <Inform id="name" label="Username"></Inform>
-        <Inform id="age" label="Age (Years)"></Inform>
+        <div className={styles.input}>
+          <label for="username">Username</label>
+          <input type="text" id="username" onChange={saveUsername}></input>
+        </div>
+        <div className={styles.input}>
+          <label for="age">Age</label>
+          <input type="text" id="age" onChange={saveAge}></input>
+        </div>
         <Button label="Add User"></Button>
       </form>
     </div>
